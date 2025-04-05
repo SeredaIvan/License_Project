69d99b5e22b4232b183c21c42b9fe28fa453b98b
SeredaIvan <vanosereda77@gmail.com>
2025-04-05 21:40:01 +0300

    added dev db and change seeder

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url:
+        process.env.NODE_ENV === "production"
+          ? process.env.POSTGRES_PRISMA_URL_PROD
+          : process.env.POSTGRES_PRISMA_URL_DEV,
+    },
+  },
+});
 
 async function main() {
+  
   console.log("Seeding database...");
 
   const users = Array.from({ length: 100 }, () => ({
