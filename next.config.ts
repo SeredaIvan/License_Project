1331cc17c1cc38ba0c7ac2cb58b6aabf4dd5553a
SeredaIvan <vanosereda77@gmail.com>
2025-05-13 21:40:51 +0300

    remove redirects next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/docs',
-                destination: '/docs/index.html',
-                permanent: false,
-            },
-        ];
-    },
 };
 
 export default nextConfig;
